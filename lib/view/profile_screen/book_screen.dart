27c7923af3e2ca823d196d1b753fc9b31fd2e2a0
m6696282 <ebrahemimorteza10@gmail.com>
2025-06-04 14:46:10 +0330

    add book me

---
+++ lib/view/profile_screen/book_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+class BookScreen extends StatefulWidget {
+  const BookScreen({super.key});
+
+  @override
+  State<BookScreen> createState() => _BookScreenState();
+}
+
+class _BookScreenState extends State<BookScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
+    return Padding(
+      padding: EdgeInsets.only(top: size.height*0.3),
+      child: Container(
+          decoration: BoxDecoration(
+              color: CupertinoColors.white,
+              borderRadius: BorderRadius.all(Radius.circular(30.0))
+          )
+      ),
+    );
+  }
+}
